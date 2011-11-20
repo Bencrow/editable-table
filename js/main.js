53971be916c7520ba5d6cff01880038dede0fa49
b2l <b2l.powa@gmail.com>
2011-11-20 16:25:18 +0100

    Directory structure and main file

---
+++ js/main.js
@@ -0,0 +1,17 @@
+YUI().use("node", "event", "widget", function(Y) {
+
+    // Load our main widget
+    
+    /*
+     * This is an example of the configuration that would be great
+     */
+    /*
+    var editableTable = new Y.EditableTable({
+        srcNode:        "#table",
+        headerEditable: false,
+        addColumn:      false,
+        addRow:         false,
+        resizable:      false,
+    });
+    */
+});

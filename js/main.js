641820fe4045db1f03d7e8641698afadcd08f39f
b2l <b2l.powa@gmail.com>
2011-11-20 20:03:23 +0100

    Add local yui and twitter bootstrap. Modify link in index.html . Set the base YUI Config

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-YUI().use("node", "event", "widget", function(Y) {
-
-    // Load our main widget
-    
-    /*
-     * This is an example of the configuration that would be great
-     */
-    /*
-    var editableTable = new Y.EditableTable({
-        srcNode:        "#table",
-        headerEditable: false,
-        addColumn:      false,
-        addRow:         false,
-        resizable:      false,
-    });
-    */
+YUI({
+    lang: "fr",
+    combine: false,
+    groups: {
+        "widget": {
+            combine: false,
+            base: "js/widgets/",
+            modules: {
+                "editableTable": {
+                    path: "editableTable.js",
+                    require: ["node", "event", "widget", "substitute"] 
+                }
+            }
+        }
+    },
+}).use("widget", function(Y) {
 });
